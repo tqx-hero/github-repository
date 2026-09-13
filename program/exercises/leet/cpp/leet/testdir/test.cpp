5351d2c398712819bbc105af7be56ca5ec272c40
tqx-hero <18615230761@163.com>
2026-09-13 19:35:28 +0800

    leet

--- program/exercises/leet/cpp/leet/testdir/test.cpp
+++ program/exercises/leet/cpp/leet/testdir/test.cpp
@@ -1,14 +1,16 @@
 /**
- * 33. 搜索旋转排序数组
+ * 153. 寻找旋转排序数组中的最小值
 中等
 相关标签
 premium lock icon
 相关企业
-整数数组 nums 按升序排列，数组中的值 互不相同 。
+提示
+已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
 
-在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 向左旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 下标 3 上向左旋转后可能变为 [4,5,6,7,0,1,2] 。
-
-给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
 
 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
 
@@ -16,65 +18,54 @@ premium lock icon
 
 示例 1：
 
-输入：nums = [4,5,6,7,0,1,2], target = 0
-输出：4
+输入：nums = [3,4,5,1,2]
+输出：1
+解释：原数组为 [1,2,3,4,5] ，旋转 3 次得到输入数组。
 示例 2：
 
-输入：nums = [4,5,6,7,0,1,2], target = 3
-输出：-1
+输入：nums = [4,5,6,7,0,1,2]
+输出：0
+解释：原数组为 [0,1,2,4,5,6,7] ，旋转 4 次得到输入数组。
 示例 3：
 
-输入：nums = [1], target = 0
-输出：-1
+输入：nums = [11,13,15,17]
+输出：11
+解释：原数组为 [11,13,15,17] ，旋转 4 次得到输入数组。
  
 
 提示：
 
-1 <= nums.length <= 5000
--104 <= nums[i] <= 104
-nums 中的每个值都 独一无二
-题目数据保证 nums 在预先未知的某个下标上进行了旋转
--104 <= target <= 104
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+nums 中的所有整数 互不相同
+nums 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
  */
 #include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 class Solution {
-    int binary_search_target(vector<int>&nums,int left,int right,int target){
-        if(left > right)
-            return -1;
-        //如果左边界小于右边界，该区间严格单调递增，直接二分查找是否存在
-        if(nums[left] < nums[right]){
-            auto it =  lower_bound(nums.begin()+left,nums.begin()+right+1,target);
-            if(it == nums.end() || *it != target)
-                return -1;
-            return static_cast<int>(it - nums.begin());
-        }else{
-            //左边界大于右边界，该区间有凹陷
-            int mid = left + (right - left) /2;
-            int mid_val = nums[mid],left_val = nums[left];
-            if(mid_val == target)
-                return mid;
-            if((left_val > mid_val && (mid_val > target || left_val <= target))
-                    ||
-                    (left_val < mid_val && mid_val > target && left_val <= target))
-                return binary_search_target(nums,left,mid-1,target);     
-            return binary_search_target(nums,mid+1,right,target);
-        }
+    int get_minval(vector<int>& nums,int left,int right){
+        int left_val =nums[left];
+        if(left_val <= nums[right])
+            return left_val;
+        int mid = left + (right -left) / 2;
+        //中值大于左值，左区间严格递增，考察右区间
+        if(nums[mid] > left_val)
+            return get_minval(nums,mid+1,right);
+        return get_minval(nums,left+1,mid);
     }
 public:
-    int search(vector<int>& nums, int target) {
-        return binary_search_target(nums,0,nums.size()-1,target);
+    int findMin(vector<int>& nums) {
+        return get_minval(nums,0,nums.size()-1);
     }
 };
 
-// int main(){
-//     //nums = [4,5,6,7,0,1,2], target = 0
-//     vector<int> nums{4,5,6,7,0,1,2};
-//     int target =0;
-//     Solution sl;
-//     cout << sl.search(nums,target) << endl;
-//     return 0;
-// }
+int main(){
+    // vector<int> nums{3,4,5,1,2};
+    vector<int> nums{4,5,6,7,0,1,2};
+    Solution sl;
+    cout << sl.findMin(nums) << endl;
+    return 0;
+}
---
+++ program/exercises/leet/cpp/leet/热门100题/寻找旋转排序数组中的最小值-153.cpp
@@ -0,0 +1,70 @@
+/**
+ * 153. 寻找旋转排序数组中的最小值
+中等
+相关标签
+premium lock icon
+相关企业
+提示
+已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+
+给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+
+你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+
+ 
+
+示例 1：
+
+输入：nums = [3,4,5,1,2]
+输出：1
+解释：原数组为 [1,2,3,4,5] ，旋转 3 次得到输入数组。
+示例 2：
+
+输入：nums = [4,5,6,7,0,1,2]
+输出：0
+解释：原数组为 [0,1,2,4,5,6,7] ，旋转 4 次得到输入数组。
+示例 3：
+
+输入：nums = [11,13,15,17]
+输出：11
+解释：原数组为 [11,13,15,17] ，旋转 4 次得到输入数组。
+ 
+
+提示：
+
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+nums 中的所有整数 互不相同
+nums 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
+ */
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+    int get_minval(vector<int>& nums,int left,int right){
+        int left_val =nums[left];
+        if(left_val <= nums[right])
+            return left_val;
+        int mid = left + (right -left) / 2;
+        //中值大于左值，左区间严格递增，考察右区间
+        if(nums[mid] > left_val)
+            return get_minval(nums,mid+1,right);
+        return get_minval(nums,left+1,mid);
+    }
+public:
+    int findMin(vector<int>& nums) {
+        return get_minval(nums,0,nums.size()-1);
+    }
+};
+
+int main(){
+    vector<int> nums{3,4,5,1,2};
+    Solution sl;
+    cout << sl.findMin(nums) << endl;
+    return 0;
+}
